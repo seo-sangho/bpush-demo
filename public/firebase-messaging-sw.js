35764fdce12c03cfaa35678f9419710464bebbff
seo-sangho <ssh0702@gmail.com>
2024-11-15 15:42:18 +0900

    Feat: 메세지백그라운드수신 추가

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -11,3 +11,14 @@ const firebaseApp = firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+messaging.onBackgroundMessage((payload) => {
+  console.log(`onBackgroundMessage: ${payload}`);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.icon,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
